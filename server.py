e0f681b154b0914daf47c0eff69b92e5b757937b
Daniel-Iofin <daniel.iofin26@trinityschoolnyc.org>
2023-09-22 19:43:02 -0400

    Made basic website template

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+import json
+
+app = Flask(__name__, static_url_path='', static_folder='static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(debug=True)
