97e832d563ca48eaa24c58e8d66b90a45d9b7ef1
Daniel-Iofin <130488723+Daniel-Iofin@users.noreply.github.com>
2023-09-24 16:06:07 -0400

    Added functionality to new-listing.html

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-import json
-
+import sqlite3
+from random import randint
 app = Flask(__name__, static_url_path='', static_folder='static')
+
+connect = sqlite3.connect("bookDB.db")
 
 @app.route('/')
 def index():
@@ -13,8 +15,21 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/new-listing')
+@app.route('/new-listing', methods=['GET', 'POST'])
 def newListing():
+    if request.method == 'POST':
+        email = request.form['email']
+        title = request.form['title']
+        course = request.form['course']
+        condition = request.form['condition']
+        price = request.form['price']
+        info = request.form['info']
+
+        with sqlite3.connect("bookDB.db") as bookDB:
+            cursor = bookDB.cursor()
+            cursor.execute("INSERT INTO listings values(?, ?, ?, ?, ?, ?, ?)", (randint(0, 9223372036854775807), email, title, course, condition, price, info))
+            bookDB.commit()
+        
     return render_template('new-listing.html')
 
 app.run(debug=True)
